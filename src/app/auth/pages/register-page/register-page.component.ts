bcf675adfaf2d98269e3a13cb448af45fd642809
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-11-12 23:19:00 -0500

    validator service

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from '../../../shared/services/validators.service';
 
-import {
-  canBeStride,
-  emailPattern,
-  firstNameAndLastnamePattern,
-} from '../../../shared/validations/validatots';
+// import {
+//   canBeStride,
+//   emailPattern,
+//   firstNameAndLastnamePattern,
+// } from '../../../shared/validations/validatots';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -15,18 +16,30 @@ export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
     name: [
       '',
-      [Validators.required, Validators.pattern(firstNameAndLastnamePattern)],
+      [
+        Validators.required,
+        Validators.pattern(this.validatorService.firstNameAndLastnamePattern),
+      ],
     ],
-    email: ['', [Validators.required, Validators.pattern(emailPattern)]],
-    username: ['', [Validators.required, canBeStride]],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(this.validatorService.emailPattern),
+      ],
+    ],
+    username: ['', [Validators.required, this.validatorService.canBeStride]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     confirmPassword: ['', [Validators.required]],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private validatorService: ValidatorsService
+  ) {}
 
-  isValidField(field: string) {
-    // TODO: get validatins from service provider
+  isValidField(field: string): boolean | null {
+    return this.validatorService.isValidField(this.myForm, field);
   }
 
   onSubmit() {
