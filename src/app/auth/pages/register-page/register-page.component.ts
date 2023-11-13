43ae1e75f11ba2076740c827958e9699ef5d26f2
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-11-12 22:58:01 -0500

    create patterns for email -  compound name and func cantBeStride

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import {
+  canBeStride,
+  emailPattern,
+  firstNameAndLastnamePattern,
+} from '../../../shared/validations/validatots';
+
 @Component({
   templateUrl: './register-page.component.html',
   styles: [],
 })
 export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required]],
+    name: [
+      '',
+      [Validators.required, Validators.pattern(firstNameAndLastnamePattern)],
+    ],
+    email: ['', [Validators.required, Validators.pattern(emailPattern)]],
+    username: ['', [Validators.required, canBeStride]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     confirmPassword: ['', [Validators.required]],
   });
